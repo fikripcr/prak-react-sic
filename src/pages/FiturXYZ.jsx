1934aa15f3d2eed51f1ed10c355c43c2923489e6
fikripcr <fikri@pcr.ac.id>
2026-06-04 15:37:44 +0700

    feat: tambah komponen navbar

--- src/pages/FiturXYZ.jsx
+++ src/pages/FiturXYZ.jsx
@@ -1,12 +1,55 @@
 import { FaShoppingCart, FaTruck, FaBan, FaDollarSign } from "react-icons/fa";
 import PageHeader from "../components/PageHeader";
-import Card from "../components/Card";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+import { Badge } from "@/components/ui/badge";
 
 export default function FiturXYZ() {
-    return (
-        <div id="dashboard-container">
-            <PageHeader  title="Dashboard"/>
-            <p>Ini halaman Fitur XYZ</p>
-        </div>
-    );
+  return (
+    <div id="dashboard-container">
+      <PageHeader title="Dashboard" />
+      <p>Ini halaman Fitur XYZ</p>
+
+      <div>
+        <Button>Simpan</Button>
+        <Button variant="outline">Simpan</Button>
+        <Button variant="secondary">Simpan</Button>
+        <Button variant="default" size="xs">
+          Simpan
+        </Button>
+      </div>
+      <div className="mt-6">
+        <Card>
+          <CardHeader>
+            <CardTitle>Card Title</CardTitle>
+            <CardDescription>Card Description</CardDescription>
+            <CardAction>Card Action</CardAction>
+          </CardHeader>
+          <CardContent>
+            <p>Card Content</p>
+          </CardContent>
+          <CardFooter>
+            <p>Card Footer</p>
+          </CardFooter>
+        </Card>
+      </div>
+
+      <div className="mt-6">
+        <Badge>Default</Badge>
+        <Badge variant="secondary">Secondary</Badge>
+        <Badge variant="destructive">Destructive</Badge>
+        <Badge variant="outline">Outline</Badge>
+        <Badge variant="ghost">Ghost</Badge>
+      </div>
+    </div>
+  );
 }
